36990c44ad2ba63bb5f5f16a9f9b23cf80f3f4ce
Mudassar_Iqbal <160554870+Mudassar-Raju@users.noreply.github.com>
2024-07-20 21:16:06 +0500

    Delete Class-08 directory

--- Class-08/for-in-loop.ts
+++
@@ -1,9 +0,0 @@
-//Basic syntax of for in loop
-//for (variable in loop){
-  //statement
-//}
-const groceryItems :string[] = ["bread","milk","apple","juice"];
-for(const item in groceryItems){
-  console.log(`i have purchase ${groceryItems[item]}`);
-  
-};
--- Class-08/loop2.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//Table 20
-for (let i = 1; i <= 10; i++) {
-    console.log(`20 X ${i} = ${20 * i}`);
-}
-for (let i = 1; i <= 10; i++) {
-    console.log(`20 X ${i} = ${20 * i}`);
-}
